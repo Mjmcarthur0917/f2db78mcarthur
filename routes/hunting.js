4ad7e3a92e01445f9a976694572528abc4185179
Matthew McArthur <mjmcarthur0917@gmail.com>
2022-11-18 09:33:12 -0600

    added button for part 1

--- routes/hunting.js
+++ routes/hunting.js
@@ -1,10 +1,18 @@
 var express = require('express');
-const hunting_controlers= require('../controllers/hunting');
+const hunting_controllers= require('../controllers/hunting');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. 
 router.get('/', function(req, res, next) {
-  router.get('/', hunting_controlers.hunting_view_all_Page );
+  router.get('/', hunting_controllers.hunting_view_all_Page );
 });
+*/
+router.get('/detail', hunting_controllers.hunting_view_one_Page);
+
+router.get('/create', hunting_controllers.hunting_create_Page);
+
+router.get('/update', hunting_controllers.hunting_update_Page);
+
+router.get('/delete', hunting_controllers.hunting_delete_Page);
 
 module.exports = router;
